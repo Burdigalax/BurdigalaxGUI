54caf805af6263aca818b79bf376dd0610d2209f
Burdigalien <51970896+Burdigalien@users.noreply.github.com>
2020-01-05 23:51:05 +0100

    fix(inventory): fix rounding header total weight ?

--- src/app/inventory/redux/reducers/entities/inventories/getters/get-busy-storage.js
+++ src/app/inventory/redux/reducers/entities/inventories/getters/get-busy-storage.js
@@ -1,9 +1,10 @@
 import { prop, pipe, map, sum, values } from "ramda";
 
 import getItemsCompletedFromCurrentInventory from "./get-items-completed-from-current-inventory";
+import { formatNumber } from "../helpers";
 
 export default state => {
   const items = getItemsCompletedFromCurrentInventory(state);
 
-  return pipe(values, map(prop("totalWeight")), sum)(items);
+  return pipe(values, map(prop("totalWeight")), sum, formatNumber)(items);
 };
--- src/app/inventory/redux/reducers/entities/inventories/helpers/index.js
+++ src/app/inventory/redux/reducers/entities/inventories/helpers/index.js
@@ -1,7 +1,7 @@
 import selectItemById from "../../items/selectors/select-items-by-id";
 import { pathOr } from "ramda";
 
-const formatNumber = value => parseFloat(value.toFixed(2));
+export const formatNumber = value => parseFloat(value.toFixed(2));
 
 export const getFormatedItemById = (state, id, itemInventory) => {
   const item = selectItemById(state, id);
